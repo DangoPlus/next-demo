264466218b78ebe22241ba0c2eed4bc473c5c58d
邓鹏宇 <dengpengyu@dengpengyudeMacBook-Pro.local>
2021-11-02 15:37:33 +0800

    try

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -1,8 +1,13 @@
 import React, { FC, useEffect, useState } from "react";
+import { NextPage } from "next";
 import { Card } from "antd";
 import styles from "./index.module.less";
 
-const Demo = (props: any) => {
+interface Props {
+  nameList?: any;
+}
+
+const Demo: NextPage<Props> = (props) => {
   // const [nameList, setNameList] = useState([]);
   // useEffect(() => {
   //   fetch("/api/hello", { method: "POST" })
@@ -32,11 +37,19 @@ const Demo = (props: any) => {
   );
 };
 
-Demo.getInitialProps = async () => {
-  const res = await fetch("/api/hello", { method: "POST" })
-  const json = await res.json()
-  return { nameList: json.data }
-}
-
+Demo.getInitialProps = async (ctx) => {
+  if (ctx.req) {
+    // ctx.req exists server-side only
+    // call docker-host:port
+    const res = await fetch("http://lunatek.xyz:3000/api/hello", { method: "POST" });
+    const json = await res.json();
+    return { nameList: json.data };
+  } else {
+    // call localhost:port
+    const res = await fetch("/api/hello", { method: "POST" });
+    const json = await res.json();
+    return { nameList: json.data };
+  }
+};
 
 export default Demo;
