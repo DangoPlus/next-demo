6a47399bca84241b79b82d41ee154af5c2f1d370
邓鹏宇 <dengpengyu@dengpengyudeMacBook-Pro.local>
2021-11-02 15:24:15 +0800

    using getInitialProps for ssr

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -2,20 +2,20 @@ import React, { FC, useEffect, useState } from "react";
 import { Card } from "antd";
 import styles from "./index.module.less";
 
-const Demo: FC = () => {
-  const [nameList, setNameList] = useState([]);
-  useEffect(() => {
-    fetch("/api/hello", { method: "POST" })
-      .then((res) => res.json())
-      .then((response: any) => {
-        console.log(response);
-        setNameList(response.data);
-      });
-  }, []);
+const Demo = (props: any) => {
+  // const [nameList, setNameList] = useState([]);
+  // useEffect(() => {
+  //   fetch("/api/hello", { method: "POST" })
+  //     .then((res) => res.json())
+  //     .then((response: any) => {
+  //       console.log(response);
+  //       setNameList(response.data);
+  //     });
+  // }, []);
 
   return (
     <div className={styles.Demo}>
-      {nameList.map((item: any, index: any) => {
+      {props.nameList.map((item: any, index: any) => {
         return (
           <Card
             key={index}
@@ -31,5 +31,12 @@ const Demo: FC = () => {
     </div>
   );
 };
+
+Demo.getInitialProps = async () => {
+  const res = await fetch("/api/hello", { method: "POST" })
+  const json = await res.json()
+  return { nameList: json.data }
+}
+
 
 export default Demo;
